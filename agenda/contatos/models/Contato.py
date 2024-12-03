e8b1251ebbd66574d36224f1cefd65958fc94e11
Salu <saluorodrigues@gmail.com>
2024-12-02 23:11:32 -0300

    edit1

--- agenda/contatos/models/Contato.py
+++ agenda/contatos/models/Contato.py
@@ -5,6 +5,5 @@ class Contato(models.Model):
     id = models.BigAutoField(primary_key=True)
     nome = models.CharField(max_length=255)
 
-
     def __str__(self):
-        return f'{self.id} - {self.nome}'
+        return '{}'
